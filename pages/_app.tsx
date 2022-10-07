99b3285975a0b69a76feb082002cb34910f9f585
Johny Wills <jonathan_wills@caddi.jp>
2022-10-07 18:04:07 +0900

    feat: add snackbar component test logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SnackbarProvider } from '@Components';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <SnackbarProvider
+            anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+        >
+            <Component {...pageProps} />
+        </SnackbarProvider>
+    );
 }
 
 export default MyApp;
